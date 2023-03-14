c9f793d686f0b48f9c192f34e78aa2d07f5eb3a4
Khang <khang@Khangs-MacBook-Air.local>
2023-03-14 10:29:57 +0700

    create sidebar for project

---
+++ src/components/Club/Club.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Club = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">Câu lạc bộ thể thao tự động</h1>
+      <div className="row">
+        <div className="col-sm-6">
+          <h2>Tin tức mới nhất</h2>
+          <p>...</p>
+        </div>
+        <div className="col-sm-6">
+          <h2>Giới thiệu về câu lạc bộ</h2>
+          <p>...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Club;
